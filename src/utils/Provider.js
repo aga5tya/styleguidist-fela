f74df7ea775feb78d0f5b41e99b5278f15a9e50c
Prithvi Raju M <aga5tya@yahoo.com>
2017-06-13 12:10:03 +0530

    Fix theme provider bug.

--- src/utils/Provider.js
+++ src/utils/Provider.js
@@ -1,13 +1,13 @@
-import React, { Children, isValidElement, cloneElement } from 'react'
+import React, { Component, Children, isValidElement, cloneElement } from 'react'
 import PropTypes from 'prop-types'
-import { createRenderer as createFelaRenderer, ThemeProvider } from 'fela'
-import { Provider } from 'react-fela'
+import { createRenderer as createFelaRenderer } from 'fela'
+import { Provider, ThemeProvider } from 'react-fela'
 
 //get the react devtools to work
 require('preact/devtools')
 
 // should be from another file. Use this to drive themes.
-import { Theme as themeVariables } from './Theme'
+import themeVariables  from './Theme'
 
 // default Opts, when shipped as a Provider to others, this has to be from props.
 // but styleguidist needs this at global.
@@ -46,7 +46,6 @@ const getFelaMountNode = () => {
 	}
 	// Always create a new style element to handle hot reloading.
 	// During isomorphic injection new dom is needed to sync style state.
-	// Taken from este repo.
 	const nextNode = document.createElement('style')
 	nextNode.id = 'fela-stylesheet'
 	parent.replaceChild(nextNode, node)
@@ -59,7 +58,7 @@ const felaMountNode = getFelaMountNode()
 // Use the below function to customer dev/prod mode plugins
 const felaRenderer = createRenderer({})
 
-class StyleProvider extends React.Component {
+export default class StyleProvider extends Component {
 	render() {
 		const child = Children.only(this.props.children)
 		return (
@@ -83,4 +82,3 @@ StyleProvider.propTypes = {
 }
 
 export { createRenderer }
-export default StyleProvider
